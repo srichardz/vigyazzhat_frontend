03ce4497e1d8b6cddc0b767ea76c891323babacd
Riccsard <58710642+Riccsard@users.noreply.github.com>
2024-11-12 18:54:57 +0100

    frontend

--- vigyazzhat_frontend/src/App.js
+++ vigyazzhat_frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [data, setdata] = useState({
+    name: "username",
+  });
+
+  useEffect(() => {
+    // Using fetch to fetch the api from 
+    // flask server it will be redirected to proxy
+    fetch("/test").then((res) =>
+        res.json().then((data) => {
+            // Setting a data from api
+            setdata({
+                name: data.name,
+            });
+        })
+    );
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>Eredmeny: {data.name}</p>
     </div>
   );
 }
